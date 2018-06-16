ffe1b9ba485b0e39bd68eb4b5fc9ec073e3fefd0
anjin.haj <anjin.haj@alibaba-inc.com>
2018-06-16 15:21:24 +0800

    del cache

--- script/task.js
+++ script/task.js
@@ -1,38 +1,3 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  '/test/',
-  '/test/styles/main.css',
-  '/test/script/main.js'
-];
-
-self.addEventListener('install', function (event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function (cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    // 以下方法检视请求，并从服务工作线程所创建的任何缓存中查找缓存的结果。
-    caches.match(event.request)
-      .then(function (response) {
-        console.log(event.request)
-        console.log(caches)
-        // 如果发现匹配的响应，则返回缓存的值
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-      )
-  );
-});
-
 self.addEventListener('message', function (event) {
   var promise = self.clients.matchAll()
     .then(function (clientList) {
