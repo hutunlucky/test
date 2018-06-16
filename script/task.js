dc913282d904861ca334cd1e35bc7856fdf0d20d
anjin.haj <anjin.haj@alibaba-inc.com>
2018-06-16 13:52:00 +0800

    add fetch 事件

--- script/task.js
+++ script/task.js
@@ -5,6 +5,24 @@ var urlsToCache = [
   '/test/script/main.js'
 ];
 
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // 以下方法检视请求，并从服务工作线程所创建的任何缓存中查找缓存的结果。
+    caches.match(event.request)
+      .then(function(response) {
+        console.log(event.request)
+        console.log(caches)
+        // 如果发现匹配的响应，则返回缓存的值
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
