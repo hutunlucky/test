3a4051796530b1f463aeafe85dd5526b7c35f2f5
anjin.haj <anjin.haj@alibaba-inc.com>
2018-06-16 15:30:30 +0800

    fallback

--- script/task.js
+++ script/task.js
@@ -1,27 +1,64 @@
-self.addEventListener('message', function (event) {
-  var promise = self.clients.matchAll()
-    .then(function (clientList) {
-      var senderId = event.source ? event.source.id : 'unknown';
-      if (!event.source) {
-        console.log('event.source is null; we don\'t know the sender of the message');
-      }
-      clientList.forEach(function (client) {
-        if (client.id !== senderId) {
-          return;
-        }
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/test/',
+  '/test/styles/main.css',
+  '/test/script/main.js'
+];
 
-        client.postMessage({
-          client: senderId,
-          message: event.data
-        })
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // 以下方法检视请求，并从服务工作线程所创建的任何缓存中查找缓存的结果。
+    caches.match(event.request)
+      .then(function(response) {
+        console.log(event.request)
+        console.log(caches)
+        // 如果发现匹配的响应，则返回缓存的值
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
       })
-    })
+  );
+});
+
+self.addEventListener('message', function(event) {
+  var promise = self.clients.matchAll()
+      .then(function(clientList) {
+          var senderId = event.source ? event.source.id: 'unknown';
+          if (!event.source) {
+              console.log('event.source is null; we don\'t know the sender of the ' +
+          'message');
+          }
+          clientList.forEach(function(client) {
+              if (client.id !== senderId) {
+                  return;
+              }
+
+              client.postMessage({
+                  client: senderId,
+                  message: event.data
+              })
+          })
+      })
   if (event.waitUntil) {
-    event.waitUntil(promise);
+      event.waitUntil(promise);
   }
 });
 
 
-self.addEventListener('activate', function (event) {
+self.addEventListener('activate', function(event) {
   event.waitUntil(self.clients.claim());
 })
